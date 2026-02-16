b5eadbae80f50e820458554134943c1fd05e08a8
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-02-16 23:12:22 +0530

    Update Amazon.java

--- src/main/java/main/Amazon.java
+++ src/main/java/main/Amazon.java
@@ -6,10 +6,15 @@ import Service.IDeliveryService;
 public class Amazon {
     private IDeliveryService service;
 
-    public void setService(IDeliveryService service) {
-        this.service = service;
-    }
 
+    //Through Setter Intjection
+    // public void setService(IDeliveryService service) {
+    //     this.service = service;
+    // }
+
+    public Amazon(IDeliveryService service){
+        this.service=service;
+    }
     public Boolean deliverTheProduct(Double amount){
         return service.deliverProduct(amount);
     }
