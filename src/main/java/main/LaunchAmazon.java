0a5f8f5f828ba15f051826f95d7a05b6b8a1f242
Aman Choubey <122197295+AmanKumarChoubey@users.noreply.github.com>
2026-02-16 23:13:21 +0530

    Update LaunchAmazon.java

--- src/main/java/main/LaunchAmazon.java
+++ src/main/java/main/LaunchAmazon.java
@@ -5,9 +5,9 @@ import Service.IDeliveryService;
 
 public class LaunchAmazon{
     public static void main(String[] args) {
-        Amazon amazon=new Amazon();
+        Amazon amazon=new Amazon(new BlueDart()); //Through Constructor Injection
         //Setter Injection
-        amazon.setService(new FedEx());
+        // amazon.setService(new FedEx());
         Boolean flag= amazon.deliverTheProduct(2090.0);
         if(flag) System.out.println("Product delivered Successfull");
         else System.out.println("Faild to Deliver Product");
